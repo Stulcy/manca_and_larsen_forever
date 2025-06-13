795fa307f7c01a65684e483cc2c1c0b232fb12e1
Stulcy <rokkstular@gmail.com>
2025-06-13 22:49:34 +0200

    server removed

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
   base: "/manca_and_larsen_forever/",
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean
